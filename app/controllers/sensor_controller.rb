9bd78f3bd463b22804c6fff03e9761656f5b5cfa
wsizoo <will.sizoo@appfolio.com>
2015-03-05 14:39:58 -0800

    update view

--- app/controllers/sensor_controller.rb
+++ app/controllers/sensor_controller.rb
@@ -1,6 +1,6 @@
 class SensorController < ApplicationController
   def index
     @data = Sensor.limit(30).order("created_at desc")
-    @data_status = Sensor.all.order("created_at desc").first
+    @data_status = Sensor.limit(20).order("created_at desc")
   end
 end
