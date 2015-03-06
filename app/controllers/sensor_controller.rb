a3b37f36dea6fe500691a4073ad02e864c323f51
wsizoo <will.sizoo@appfolio.com>
2015-03-05 18:58:29 -0800

    add admin view, increase number of open entries

--- app/controllers/sensor_controller.rb
+++ app/controllers/sensor_controller.rb
@@ -1,7 +1,13 @@
 class SensorController < ApplicationController
   def index
     @data = Sensor.limit(30).order("created_at desc")
-    @data_status = Sensor.limit(5).order("created_at desc")
+    @data_status = Sensor.limit(8).order("created_at desc")
+    # @data_status = Sensor.select{ |item| item[:status] == 'OPEN' }.count
+
+  end
+  def admin
+    @data = Sensor.limit(30).order("created_at desc")
+    @data_status = Sensor.limit(8).order("created_at desc")
     # @data_status = Sensor.select{ |item| item[:status] == 'OPEN' }.count
 
   end
